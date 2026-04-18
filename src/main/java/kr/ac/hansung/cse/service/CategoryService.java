bdf444b2096c54617a747725d5ea787a45989b88
jaemo <tjdwkddl@gmail.com>
2026-04-19 02:16:50 +0900

    feat : 기본 틀만 작성

--- src/main/java/kr/ac/hansung/cse/service/CategoryService.java
+++ src/main/java/kr/ac/hansung/cse/service/CategoryService.java
@@ -1,4 +1,23 @@
 package kr.ac.hansung.cse.service;
 
+import kr.ac.hansung.cse.model.Category;
+import kr.ac.hansung.cse.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 }
