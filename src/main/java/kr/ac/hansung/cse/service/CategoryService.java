33c6afd685bd4c463c53289c3175658af0c799f5
jaemo <tjdwkddl@gmail.com>
2026-04-19 02:26:31 +0900

    feat:create delete login 추가

--- src/main/java/kr/ac/hansung/cse/service/CategoryService.java
+++ src/main/java/kr/ac/hansung/cse/service/CategoryService.java
@@ -1,5 +1,6 @@
 package kr.ac.hansung.cse.service;
 
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
 import kr.ac.hansung.cse.model.Category;
 import kr.ac.hansung.cse.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,18 @@ public class CategoryService {
 
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
+    }
+
+    public Category createCategory(String name) {
+        categoryRepository.findByName(name)
+                .ifPresent(c -> { throw new DuplicateCategoryException(name); });
+        return categoryRepository.save(new Category(name));
+    }
+
+    public void deleteCategory(Long id) {
+        long count = categoryRepository.countProductsByCategoryId(id);
+        if (count > 0) throw new IllegalStateException(
+                "상품 " + count + "개가 연결되어 있어 삭제할 수 없습니다.");
+        categoryRepository.delete(id);
     }
 }
