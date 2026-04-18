6d3d634957a657e521553419eced5d241c0a5fc2
jaemo <tjdwkddl@gmail.com>
2026-04-19 02:32:02 +0900

    fix: lombok getter setter 추가

--- src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -2,8 +2,11 @@ package kr.ac.hansung.cse.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 public class CategoryForm {
+    @Getter @Setter
     @NotBlank(message = "카테고리 이름을 입력하세요.")
     @Size(max = 50, message = "50자 이내로 입력하세요")
     private String name;
