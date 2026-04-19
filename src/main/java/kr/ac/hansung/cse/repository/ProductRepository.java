faeff60fb98ad623ea3c7088a44ca56b683665c0
jaemo <tjdwkddl@gmail.com>
2026-04-19 12:24:51 +0900

    feat:검색 기능 추가

--- src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
+++ src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
@@ -131,4 +131,17 @@ public class ProductRepository {
             entityManager.remove(product);
         }
     }
+
+    public List<Product> findByNameContaining(String keyword) {
+        return entityManager.createQuery("SELECT p FROM Product p WHERE p.name LIKE :keyword", Product.class)
+                .setParameter("keyword", "%" + keyword + "%")
+                .getResultList();
+    }
+
+    public List<Product> findByCategory(Long CategoryId) {
+        return entityManager.createQuery(
+                "SELECT p FROM Product p WHERE p.category.id = :cid",Product.class)
+                .setParameter("cid", CategoryId)
+                .getResultList();
+    }
 }
